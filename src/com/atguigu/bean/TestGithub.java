b807d4aea93e288d8c3cb2c971c8cd1e7dce282e
孙博宇 <473799910@qq.com>
2020-02-13 11:27:46 +0800

    love from weakfool

---
+++ src/com/atguigu/bean/TestGithub.java
@@ -0,0 +1,10 @@
+package com.atguigu.bean;
+
+public class TestGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              System.out.println("love from weakfool");
+	}
+
+}
